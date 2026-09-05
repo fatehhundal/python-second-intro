22692eb2a51a02425145e512be21e5d7c3aba94b
fatehhundal <fshundal@hotmail.com>
2026-09-05 12:28:09 +0800

    assignment

---
+++ assignment.py
@@ -0,0 +1,8 @@
+print("Hi. Welcome to the program.")
+
+name = input("I want to know more about you. What's your name? ")
+location = input("What is the nearest city to you? ")
+sport = input("What is your favourite sport? ")
+
+print("Okay", name, "I will be finding the nearest", sport, "games in", location + "...\n")
+print("There are no results,\nsince I haven't fully developed.")
